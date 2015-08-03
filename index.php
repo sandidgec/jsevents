c82ddca58faf5a90589eb236520400896a109e0c
Charles Sandidge <sandidgec@gmail.com>
2015-08-03 15:25:43 -0600

    adding JS to change font and highlight color

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 	<head>
 		<meta charset="utf-8"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
@@ -31,17 +31,25 @@
 
 		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-		<title>Bootstrap 101 Template (DDC Version!)</title>
-	</head>
-	<body>
-		<div class="container">
-			<h1> JavaScript Example Page</h1>
 
-			<p>
+		<script type="text/javascript" src="js/jsexample.js"></script>
+		<title>JavaScript Example Page</title>
+	</head>
+
+	<body>
+
+
+			<div id ="fontchange" onclick="clickEvent();">
+				<h1> JavaScript Example Page </h1>
+			</div>
+
+			<div id="highlight" onmouseover="mouseOver(); "onmouseout="mouseOut();">
+				<p>
 				Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?
 				Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that
 				shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy
 				store, because you're here shopping for an infant named Jeb. </p>
+			</div>
 
 			<p>
 				Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you
@@ -56,6 +64,7 @@
 				when I lay My vengeance upon thee. </p>
 
 		</div>
+
 	</body>
 </html>
 
@@ -64,22 +73,3 @@
 
 
 
-
-
-<p>
-	Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?
-	Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that
-	shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy
-	store, because you're here shopping for an infant named Jeb. </p>
-
-<p>
-	Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you
-	can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends,
-	like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
-
-<p>
-	The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men.
-	Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he
-	is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance
-	and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord
-	when I lay My vengeance upon thee. </p>
---
+++ js/jsexample.js
@@ -0,0 +1,19 @@
+
+
+
+function clickEvent() {
+	document.getElementById("fontchange").style.fontFamily = "Comic Sans MS";
+
+
+
+}
+
+
+
+function mouseOver() {
+	document.getElementById("highlight").style.backgroundColor = "dodgerblue";
+}
+
+function mouseOut() {
+	document.getElementById("highlight").style.backgroundColor = "inherit";
+}

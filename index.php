b633fac51133e8507ffbc3b2ce389a9ea4d0c0f1
Charles Sandidge <sandidgec@gmail.com>
2015-08-03 13:25:14 -0600

    added directories and bootstrap template

---
+++ index.php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+
+		<link type="text/css" href="css/stylesheet.css" rel="stylesheet"/>
+
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<title>Bootstrap 101 Template (DDC Version!)</title>
+	</head>
+	<body>
+		<div class="container">
+			<h1> JavaScript Example Page</h1>
+
+			<p>
+				Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?
+				Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that
+				shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy
+				store, because you're here shopping for an infant named Jeb. </p>
+
+			<p>
+				Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you
+				can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends,
+				like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
+
+			<p>
+				The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men.
+				Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he
+				is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance
+				and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord
+				when I lay My vengeance upon thee. </p>
+
+		</div>
+	</body>
+</html>
+
+
+
+
+
+
+
+
+<p>
+	Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it?
+	Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that
+	shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy
+	store, because you're here shopping for an infant named Jeb. </p>
+
+<p>
+	Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you
+	can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends,
+	like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
+
+<p>
+	The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men.
+	Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he
+	is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance
+	and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord
+	when I lay My vengeance upon thee. </p>
